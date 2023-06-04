a2630a5b4dac482611635e0d642cd065515fdeb7
nehal@gather.network <nehal.shaquib21@gmail.com>
2023-06-04 16:51:28 +0530

    added all db tests

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -36,11 +36,7 @@ func TestGetEntry(t *testing.T) {
 	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, entry2)
-
-	require.Equal(t, entry1.ID, entry2.ID)
-	require.Equal(t, entry1.AccountID, entry2.AccountID)
-	require.Equal(t, entry1.Amount, entry2.Amount)
-	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, 0)
+	require.Equal(t, entry1, entry2)
 }
 
 func createEntryWithAccountId(t *testing.T, accountId int64) Entry {
--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -8,10 +8,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateTransfer(t *testing.T) {
+func CreateRandomTransfer(t *testing.T) Transfer {
 	acc1 := createRandomAccount(t)
 	acc2 := createRandomAccount(t)
-
 	arg := CreateTransferParams{
 		FromAccountID: acc1.ID,
 		ToAccountID:   acc2.ID,
@@ -21,10 +20,77 @@ func TestCreateTransfer(t *testing.T) {
 	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, transfer)
-
 	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
 	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
 	require.Equal(t, arg.Amount, transfer.Amount)
 	require.NotZero(t, transfer.ID)
 	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	CreateRandomTransfer(t)
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer := CreateRandomTransfer(t)
+	transferResponse, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transferResponse)
+	require.Equal(t, transferResponse, transfer)
+}
+
+func TestListTransfers(t *testing.T) {
+	transfer := CreateRandomTransfer(t)
+	arg := ListTransfersParams{
+		FromAccountID: transfer.FromAccountID,
+		ToAccountID:   transfer.ToAccountID,
+		Limit:         1,
+		Offset:        0,
+	}
+	tranfersList, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, tranfersList)
+	require.Equal(t, tranfersList[0], transfer)
+}
+
+func TestListTransfersBetween(t *testing.T) {
+	transfer := CreateRandomTransfer(t)
+	arg := ListTransfersBetweenParams{
+		FromAccountID: transfer.FromAccountID,
+		ToAccountID:   transfer.ToAccountID,
+		Limit:         1,
+		Offset:        0,
+	}
+	tranfersList, err := testQueries.ListTransfersBetween(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, tranfersList)
+	require.Equal(t, tranfersList[0], transfer)
+}
+
+func TestListTransfersFrom(t *testing.T) {
+	transfer := CreateRandomTransfer(t)
+	arg := ListTransfersFromParams{
+		FromAccountID: transfer.FromAccountID,
+		Limit:         1,
+		Offset:        0,
+	}
+	tranfersList, err := testQueries.ListTransfersFrom(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, tranfersList)
+	require.Equal(t, tranfersList[0], transfer)
+}
+
+func TestListTransfersTo(t *testing.T) {
+	transfer := CreateRandomTransfer(t)
+	arg := ListTransfersToParams{
+		ToAccountID: transfer.ToAccountID,
+		Limit:       1,
+		Offset:      0,
+	}
+	tranfersList, err := testQueries.ListTransfersTo(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, tranfersList)
+	require.Equal(t, tranfersList[0], transfer)
 }
