a95a8fb781cece485d09d6f0f0d7fd0771e51677
nehal@gather.network <nehal.shaquib21@gmail.com>
2023-06-11 22:41:18 +0530

    updated tests

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -8,24 +8,64 @@ import (
 )
 
 func TestTransferTx(t *testing.T) {
-	acc1 := createRandomAccount(t)
-	acc2 := createRandomAccount(t)
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	amount := int64(10)
 	store := NewStore(testDB)
-	arg := TransferTxParam{
-		FromAccountID: acc1.ID,
-		ToAccountID:   acc2.ID,
-		Amount:        10,
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	// 5 concurrent transactions
+	n := 5
+	for i := 0; i < n; i++ {
+		go func() {
+			arg := TransferTxParam{
+				FromAccountID: fromAccount.ID,
+				ToAccountID:   toAccount.ID,
+				Amount:        amount,
+			}
+
+			result, err := store.TransferTx(context.Background(), arg)
+			errs <- err
+			results <- result
+		}()
 	}
-	result, err := store.TransferTx(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, result)
-	require.Equal(t, arg.FromAccountID, result.Transfer.FromAccountID)
-	require.Equal(t, arg.ToAccountID, result.Transfer.ToAccountID)
-	require.Equal(t, arg.Amount, result.Transfer.Amount)
-	require.Equal(t, arg.FromAccountID, result.FromEntry.AccountID)
-	require.Equal(t, arg.ToAccountID, result.ToEntry.AccountID)
-	require.Equal(t, arg.Amount, -result.FromEntry.Amount)
-	require.Equal(t, arg.Amount, result.ToEntry.Amount)
+
+	// validate results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		//check transfers
+		require.NotEmpty(t, result.Transfer)
+		require.Equal(t, fromAccount.ID, result.Transfer.FromAccountID)
+		require.Equal(t, toAccount.ID, result.Transfer.ToAccountID)
+		require.Equal(t, amount, result.Transfer.Amount)
+		require.NotZero(t, result.Transfer.ID)
+		require.NotZero(t, result.Transfer.CreatedAt)
+
+		//check from entry
+		require.NotEmpty(t, result.FromEntry)
+		require.Equal(t, fromAccount.ID, result.FromEntry.AccountID)
+		require.Equal(t, amount, -result.FromEntry.Amount)
+		require.NotZero(t, result.FromEntry.ID)
+		require.NotZero(t, result.FromEntry.CreatedAt)
+
+		//check to entry
+		require.NotEmpty(t, result.ToEntry)
+		require.Equal(t, toAccount.ID, result.ToEntry.AccountID)
+		require.Equal(t, amount, result.ToEntry.Amount)
+		require.NotZero(t, result.ToEntry.ID)
+		require.NotZero(t, result.ToEntry.CreatedAt)
+
+		//check from account
+		// require.NotEmpty(t, result.ToAccount)
+		// require.Equal(t, fromAccount.Balance, result.FromAccount.Balance-10*5)
+
+	}
 
 	// require.Equal(t, acc1.ID, result.FromAccount.ID)
 	// require.Equal(t, acc2.ID, result.ToAccount.ID)
